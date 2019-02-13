35f742d596ee727e8bb83f3e977a76aa33e6f89e
Akshay199456 <akshay.kum94@gmail.com>
2019-02-12 16:35:20 -0800

    Logout completed

--- controllers/authController.js
+++ controllers/authController.js
@@ -101,6 +101,7 @@ router.post('/login', async (req, res) => {
 });
 
 router.get('/logout', (req, res) => {
+	console.log("Logout req session: ",req.session);
   req.session.destroy((err) => {
     if(err){
       res.send(err);
